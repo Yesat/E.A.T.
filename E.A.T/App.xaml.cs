66ff1f33a7bb8b99b4b0477f4f1a189685ed3126
Marom <romain.flac@gmail.com>
2018-04-30 10:46:39 +0200

    Init project with tobii

--- E.A.T/App.xaml.cs
+++ E.A.T/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Tobii.Interaction;
+using Tobii.Interaction.Wpf;
 
 namespace E.A.T
 {
@@ -13,5 +15,26 @@ namespace E.A.T
     /// </summary>
     public partial class App : Application
     {
+        private Host _host;
+        private WpfInteractorAgent _agent;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Everything starts with initializing Host, which manages connection to the 
+            // Tobii Engine and provides all Tobii Core SDK functionality.
+            // NOTE: Make sure that Tobii.EyeX.exe is running
+            _host = new Host();
+
+            // We need to instantiate InteractorAgent so it could control lifetime of the interactors.
+            _agent = _host.InitializeWpfAgent();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            // we will close the coonection to the Tobii Engine before exit.
+            _host.DisableConnection();
+
+            base.OnExit(e);
+        }
     }
 }
